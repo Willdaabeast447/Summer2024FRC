02d04d2ab81d542645b9a71084d967ae7724fd5c
Willdaabeast447 <115595237+Willdaabeast447@users.noreply.github.com>
2024-06-18 21:15:23 -0500

    Update Photonvision.java

--- src/main/java/frc/robot/subsystems/Photonvision.java
+++ src/main/java/frc/robot/subsystems/Photonvision.java
@@ -5,24 +5,40 @@
 package frc.robot.subsystems;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class Photonvision extends SubsystemBase {
   PhotonCamera Shootercamera = new PhotonCamera("photonvision");
+  PhotonCamera rearCamera = new PhotonCamera("photonvision");
   AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+  Transform3d cameraToRobot = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); //Cam mounted facing forward, half a meter forward of center, half a meter up from center.
+  Pose3d estiPose3d;
   /** Creates a new Photonvision. */
-  public Photonvision() {}
+  public Photonvision() 
+  {
+
+  }
+  public Pose3d getEsitmatedPose3d(){
+    return estiPose3d;
+  }
 
   @Override
   public void periodic() {
-    var result=Shootercamera.getLatestResult()
+    var result=rearCamera.getLatestResult();
     if (result.hasTargets())
     {
      PhotonTrackedTarget target = result.getBestTarget();
+     estiPose3d = PhotonUtils.estimateFieldToRobotAprilTag(target.getBestCameraToTarget(), aprilTagFieldLayout.getTagPose(target.getFiducialId()).get(), cameraToRobot);
       
     }
     // This method will be called once per scheduler run
